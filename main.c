f2fb0553b5526e2dc85cd4193bb92243a64e6a94
Mason Young <masondyoung@gmail.com>
2016-09-28 22:09:38 -0700

    fillmino func created, ft_memcpy.c added to repo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: myoung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/28 18:55:06 by myoung            #+#    #+#             */
-/*   Updated: 2016/09/28 20:50:09 by myoung           ###   ########.fr       */
+/*   Updated: 2016/09/28 22:07:57 by myoung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,34 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#include <string.h>
+char	*ft_memcpy(void *dst, const void *src, size_t n);
+
 #include <stdio.h>
+
+void	fillmino(char *buf)
+{
+	unsigned char i;
+
+	i = 0;
+	while (i < 16)
+	{
+		if (i % 4 == 0 && buf[i] != '#' && buf[i + 1] != '#'
+				&& buf[i + 2] != '#' && buf[i + 3] != '#')
+			ft_memcpy(buf + i, "xxxx", 4);
+		if (i < 4 && buf[i] != '#'
+			   	&& buf[i + 4] != '#'
+				&& buf[i + 8] != '#' && buf[i + 12] != '#')
+		{
+			buf[i] = 'x';
+			buf[i + 4] = 'x';
+			buf[i + 8] = 'x';
+			buf[i + 12] = 'x';
+		}
+		i++;
+	}
+}
+
 int		readmino(int fd, int offset, char* buf)
 {
 	while (offset < 16 && read(fd, buf + offset, 1) != -1)
@@ -30,6 +57,9 @@ int		readmino(int fd, int offset, char* buf)
 		else if(buf[offset] != '\n')
 			return (0); //FAILED, LINE TOO LONG
 	}
+	write(1, buf, 16);
+	write(1, "\n", 1);
+	fillmino(buf);
 	write(1, buf, 16);
 	return (1);
 }
