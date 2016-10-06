d2ca379056b88604255f606ebaa64a634ba44426
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2016-10-05 17:07:34 -0700

    EMBRACE THE BITS

--- fillit.h
+++ fillit.h
@@ -13,6 +13,8 @@
 #ifndef FILLIT_H
 # define FILLIT_H
 
+typedef t_ull unsigned long long;
+
 typedef enum	e_minotype
 {
 	LINE_V,
@@ -33,7 +35,8 @@ typedef enum	e_minotype
 	Z_V,
 	T_LEFT,
 	L_UP,
-	J_LEFT
+	J_LEFT,
+	END
 }				t_minotype;
 
 typedef struct	s_blank
@@ -44,32 +47,33 @@ typedef struct	s_blank
 
 typedef struct	s_minos
 {
-	t_minotype		minotype;
+	t_minotype		type;
 	unsigned int	width_index:2;
 	unsigned int	height_index:2;
-	t_blank			blanks[2];
+	t_ull			stamp;
 }				t_minos;
 
 t_minos			g_minos[19] = {
-	{LINE_V, 0, 3, {0, 0}},
-	{LINE_H, 3, 0, {0, 0}},
-	{L_DOWN, 2, 1, {{1, 1}, {1, 2}}},
-	{T_DOWN, 2, 1, {{1, 0}, {1, 2}}},
-	{J_DOWN, 2, 1, {{1, 0}, {1, 1}}},
-	{SQUARE, 1, 1, {0, 0}},
-	{J_RIGHT, 1, 2, {{1, 1}, {1, 2}}},
-	{Z_H, 2, 1, {{1, 0}, {0, 2}}},
-	{L_LEFT, 1, 2, {{1, 0}, {2, 0}}},
-	{T_RIGHT, 1, 2, {{0, 1}, {2, 1}}},
-	{J_UP, 2, 1, {{0, 1}, {0, 2}}},
-	{L_RIGHT, 1, 2, {{0, 1}, {1, 1}}},
-	{S_V, 1, 2, {{0, 1}, {2, 0}}},
-	{S_H, 2, 1, {{0, 0}, {1, 2}}},
-	{T_UP, 2, 1, {{0, 0}, {0, 2}}},
-	{Z_V, 1, 2, {{0, 0}, {2, 1}}},
-	{T_LEFT, 1, 2, {{0, 0}, {2, 0}}},
-	{L_UP, 2, 1, {{0, 0}, {0, 1}}},
-	{J_LEFT, 1, 2, {{0, 0}, {1, 0}}}
+	{LINE_V, 0, 3, 0x8000800080008000},
+	{LINE_H, 3, 0, 0xF000000000000000},
+	{L_DOWN, 2, 1, 0xE000800000000000},
+	{T_DOWN, 2, 1, 0xE000400000000000},
+	{J_DOWN, 2, 1, 0xE000200000000000},
+	{SQUARE, 1, 1, 0xC000C00000000000},
+	{J_RIGHT, 1, 2, 0xC000800080000000},
+	{Z_H, 2, 1, 0xC000600000000000},
+	{L_LEFT, 1, 2, 0xC000400040000000},
+	{T_RIGHT, 1, 2, 0x8000C00080000000},
+	{J_UP, 2, 1, 0x8000E00000000000},
+	{L_RIGHT, 1, 2, 0x80008000C0000000},
+	{S_V, 1, 2, 0x8000C00040000000},
+	{S_H, 2, 1, 0x6000C00000000000},
+	{T_UP, 2, 1, 0x4000E00000000000},
+	{Z_V, 1, 2, 0x4000C00080000000},
+	{T_LEFT, 1, 2, 0x4000C00040000000},
+	{L_UP, 2, 1, 0x2000E00000000000},
+	{J_LEFT, 1, 2, 0x40004000C0000000},
+	{END, 0, 0, 0};
 };
 
 #endif
