67200b4605cff70c726880a48099a25cc76d4bb6
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2016-10-05 19:56:45 -0700

    more stuff

--- fillit.h
+++ fillit.h
@@ -42,34 +42,43 @@ typedef enum	e_minotype
 typedef struct	s_mino
 {
 	t_minotype		type;
-	unsigned int	width_index:3;
-	unsigned int	height_index:3;
+	unsigned int	width_offset:3;
+	unsigned int	added_blanks:2;
 	t_ull			stamp;
 }				t_mino;
 
+typedef struct	s_grid
+{
+	t_ull		long1;
+	t_ull		long2;
+	t_ull		long3;
+	t_ull		long4;
+}				t_grid;
+
 t_mino			g_all_minos[20] = {
-	{LINE_V, 1, 4, 0x8000800080008000},
-	{LINE_H, 4, 1, 0xF000000000000000},
-	{L_DOWN, 3, 2, 0xE000800000000000},
-	{T_DOWN, 3, 2, 0xE000400000000000},
-	{J_DOWN, 3, 2, 0xE000200000000000},
-	{SQUARE, 2, 2, 0xC000C00000000000},
-	{J_RIGHT, 2, 3, 0xC000800080000000},
-	{Z_H, 3, 2, 0xC000600000000000},
-	{L_LEFT, 2, 3, 0xC000400040000000},
-	{T_RIGHT, 2, 3, 0x8000C00080000000},
-	{J_UP, 3, 2, 0x8000E00000000000},
-	{L_RIGHT, 2, 3, 0x80008000C0000000},
-	{S_V, 2, 3, 0x8000C00040000000},
-	{S_H, 3, 2, 0x6000C00000000000},
-	{T_UP, 3, 2, 0x4000E00000000000},
-	{Z_V, 2, 3, 0x4000C00080000000},
-	{T_LEFT, 2, 3, 0x4000C00040000000},
+	{LINE_V, 1, 0, 0x8000800080008000},
+	{LINE_H, 4, 0, 0xF000000000000000},
+	{L_DOWN, 3, 0, 0xE000800000000000},
+	{T_DOWN, 3, 0, 0xE000400000000000},
+	{J_DOWN, 3, 0, 0xE000200000000000},
+	{SQUARE, 2, 0, 0xC000C00000000000},
+	{J_RIGHT, 2, 0, 0xC000800080000000},
+	{Z_H, 2, 0, 0xC000600000000000},
+	{L_LEFT, 2, 0, 0xC000400040000000},
+	{T_RIGHT, 1, 0, 0x8000C00080000000},
+	{J_UP, 1, 0, 0x8000E00000000000},
+	{L_RIGHT, 1, 0, 0x80008000C0000000},
+	{S_V, 1, 0, 0x8000C00040000000},
+	{S_H, 3, 1, 0x6000C00000000000},
+	{T_UP, 2, 1, 0x4000E00000000000},
+	{Z_V, 2, 1, 0x4000C00080000000},
+	{T_LEFT, 2, 1, 0x4000C00040000000},
 	{L_UP, 3, 2, 0x2000E00000000000},
-	{J_LEFT, 2, 3, 0x40004000C0000000},
+	{J_LEFT, 2, 1, 0x40004000C0000000},
 	{END, 0, 0, 0}
 };
 
 extern t_mino	*g_minos;
+extern t_grid	g_grid;
 
 #endif
